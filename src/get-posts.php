cf347843b0d2ac8218a65c3f657e55050c9016c3
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-08-03 00:42:28 +0600

    update src/get-post.php

--- src/get-posts.php
+++ src/get-posts.php
@@ -4,10 +4,16 @@ include '../include/db.php';
 header('Content-Type: application/json');
 
 try {
+
     $stmt = $pdo->query('SELECT * FROM posts ORDER BY created_at DESC');
+
     $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
     echo json_encode(['posts' => $posts]);
+
 } catch (PDOException $e) {
+
     echo json_encode(['error' => $e->getMessage()]);
+
 }
 ?>
